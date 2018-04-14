044712e148afdd83a63f44b446375fc97dc41fdd
johrad <jpdragic@gmail.com>
2018-04-14 16:27:05 +0200

    yes

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -3,6 +3,7 @@ int led2 = 10;
 void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
@@ -12,7 +13,11 @@ void loop() {
   digitalWrite(led1, LOW);
   delay(50);     
   digitalWrite(led1, HIGH);
+  delay(50);    
+  digitalWrite(led2, HIGH);
   delay(50);                        
   digitalWrite(led1, LOW); 
   delay(3500);
+  digitalWrite(led2, LOW); 
+  delay(1000);
 }
