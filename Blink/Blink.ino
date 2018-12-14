d63f9959209c43368df16d326cb949b197bbefbe
johrad <jpdragic@gmail.com>
2018-12-14 21:03:19 +0100

    ye

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,23 +1,21 @@
+// #include <iostream>
 int led1 = 12;
 int led2 = 10;
-void setup() {
+void setup()
+{
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
 }
 
 // the loop function runs over and over again forever
-void loop() {
-  digitalWrite(led1, HIGH);
-  delay(50); 
-  digitalWrite(led1, LOW);
-  delay(50);     
-  digitalWrite(led1, HIGH);
-  delay(50);    
+void loop()
+{
   digitalWrite(led2, HIGH);
-  delay(50);                        
-  digitalWrite(led1, LOW); 
-  delay(3500);
-  digitalWrite(led2, LOW); 
-  delay(1000);
+  digitalWrite(led1, LOW);
+  delay(100);
+  digitalWrite(led2, LOW);
+  digitalWrite(led1, HIGH);
+
+  delay(100);
 }
